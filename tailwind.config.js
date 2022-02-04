ca263922750f5a69ab5fa517139916f9fc0fed17
Todd Judd <todd.judd@gmail.com>
2022-02-03 19:46:44 -0700

    Initial Landing Page (#7)
    * feat(firebase): Adding Firebase config files
    
    * feat(links): Created Icon Link Element
    
    Tested an idea for creating links to external pages with the fav icon of that page.
    It works, however the styling is weird as I'd like a black and white icon not the colored favicon.
    This is not being added to the current landing page but it's a fun idea in another style
    
    * feat(assets): Adding assets
    
    * feat(landing): Initial Landing Page
    
    Setup initial landing page
    Added Font Awesome
    updated Tailwind Font Config
    
    * fix(head): Removing react branding
    
    Swapped out to the Tracer fox favicon untill I can make a T icon
    Updated title

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      roboto: ['Roboto', 'Arial', 'sans-serif'],
+    },
     extend: {
       animation: {
         'spin-slow': 'spin 20s linear infinite',
