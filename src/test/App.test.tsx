bea8a73f9bd930aa3e529be92e2d5184dcb22f65
Todd Judd <todd.judd@gmail.com>
2022-02-04 17:34:51 -0700

    Adding Basic Testing (#10)
    * test: Adding basic tests
    
    Adding tests to ensure 3 expected links are rendered.
    
    * test(actions): Adding GH Action to run test

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -3,8 +3,21 @@ import React from 'react';
 
 import App from '../App';
 
-test('renders learn react link', () => {
+test('renders all links', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const links = screen.getAllByRole('link');
+  let expectedLinks: RegExp[] = [
+    /mailto:todd.judd@gmail.com/i,
+    /https:\/\/www.linkedin.com\/in\/todd-judd-432a86b4/i,
+    /https:\/\/github.com\/toddjudd/i,
+  ];
+  links.map((link) => {
+    expect(link.hasAttribute('href'));
+    const expectedLinkIndex = expectedLinks.findIndex((regex: RegExp) => {
+      return link.attributes.getNamedItem('href')?.value.match(regex);
+    });
+    expect(expectedLinkIndex).toBeGreaterThan(-1);
+    expectedLinks = expectedLinks.filter((_, i) => i !== expectedLinkIndex);
+  });
+  expect(expectedLinks.length).toEqual(0);
 });
